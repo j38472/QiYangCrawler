0d39fdcc759f76e3cf0035598346cb4d1ec7fb25
j38472 <34500603+j38472@users.noreply.github.com>
2021-04-16 16:56:21 +0800

    一个获取ajax 的测试

--- src/main/java/Text/ajax.java
+++ src/main/java/Text/ajax.java
@@ -1,14 +1,14 @@
 package Text;
 
 import Dao.PoJo;
-import Dao.Tool;
+import Dao.MyTool;
 
 public class ajax {
     public static void main(String[] args) {
-        Tool tool =  new Tool();
+        MyTool myTool =  new MyTool();
         String url = "https://spa6.scrape.center/api/movie/?limit=10&offset=0&token=MTZhN2VlZTFjN2EyNGM2ODhlN2FmYmEyYWE0OThmNDdkNDg5MjcwNywxNjE4MzAxMDQ5";
         String urlJs = "https://spa7.scrape.center/js/main.js";
-        PoJo poJo = tool.ClientGetHtmlPage(urlJs,"https://spa5.scrape.center/");
+        PoJo poJo = myTool.ClientGetHtmlPage(urlJs,"https://spa5.scrape.center/");
         System.out.println(poJo.getHtml());
     }
 }
