fb68cd29eaf7e68bcb8be932191503ba57b848cd
j38472 <34500603+j38472@users.noreply.github.com>
2021-04-20 10:17:11 +0800

    一些配置文档

--- src/main/java/Dao/text.java
+++ src/main/java/Dao/text.java
@@ -4,10 +4,14 @@ public class text {
     public static void main(String[] args) {
         String str = "电话：86-0551-63368968";
         String stri = "3";
-        System.out.println(str.indexOf(stri));
+//        System.out.println(str.indexOf(stri));
 
 //        MyTool myTool = new MyTool();
 //        System.out.println(myTool.checkTelephone(str));
+
+
+//        MyJDBC myJDBC = new MyJDBC();
+//        myJDBC.getSelectDb()
 
     }
 }
